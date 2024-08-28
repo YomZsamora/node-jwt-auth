e7a9d7cad72e99dbfc5d5a5eef46c73f887ece8d
yomZsamora <samaurah@gmail.com>
2024-08-28 16:43:40 +0300

    Implemented validation middleware for checking required fields for basic login.

---
+++ src/middlewares/authentication-middlewares.js
@@ -0,0 +1,17 @@
+const { check, validationResult } = require('express-validator');
+const { ValidationError } = require('../utils/exceptions/custom-exceptions');
+
+const validateLogin = [
+    check('email').isEmail.withMessage('Valid email address is required.').bail(),
+    check('password').not().isEmpty().withMessage('Password is required.').bail(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            const validationErrors = errors.array().map(err => ({ [err.path]: err.message }));
+            throw new ValidationError('An error occured while logging in.', validationErrors);
+        }
+        next();
+    }
+];
+
+module.exports = { validateLogin };
