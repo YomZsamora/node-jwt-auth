4be6559a9543aa1ae88436c6e75ab319f6243324
yomZsamora <samaurah@gmail.com>
2024-08-28 16:33:22 +0300

    Defined controller for handling basic-login functionality.

---
+++ src/controllers/authentication/basic-login-controller.js
@@ -0,0 +1,37 @@
+const User = require('../../models/authentication/user');
+const { ValidationError } = require('../../utils/exceptions/custom-exceptions');
+const { ApiResponse } = require('../../utils/responses');
+const { generateAccessToken } = require('../../utils/tokens');
+
+const basicLoginController = async (req, res, next) => {
+
+    try {
+
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: {email} });
+        if (!user) throw new ValidationError(`${email} does not exist.`);
+
+        const isValidPassword = await user.isValidPassword(password);
+        if (!isValidPassword) throw new ValidationError('Invalid email and/or password.');
+
+        user.lastLogin = new Date();
+        user.save()
+
+        const accessToken = await generateAccessToken(user);
+
+        const apiResponse = new ApiResponse();
+        apiResponse.message = "Logged in successfully.";
+        apiResponse.data = {
+            userId: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            lastLogin: user.lastLogin,
+            accessToken
+        }
+        return res.status(apiResponse.code).json(apiResponse);
+    } catch(error) {
+        next(error);
+    }
+}
+
+module.exports = { basicLoginController };
