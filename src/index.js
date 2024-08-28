568371881fba24178a08aaa13e59c3c7c2f04b7a
yomZsamora <samaurah@gmail.com>
2024-08-28 15:18:27 +0300

    Added the error-handling middleware to the app in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('./config/sequelize');
+const errorHandler = require('./utils/exceptions/exception-handler');
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Welcome to the Dockerized Node.js App!');
 });
+app.use(errorHandler);
 
 sequelize.sync({ force: false })
     .then(() => {
