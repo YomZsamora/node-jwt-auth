5e8acf6ef6696d605b020ffe192b36d145b77a8a
yomZsamora <samaurah@gmail.com>
2024-08-28 20:46:57 +0300

    Configured route for refresh tokens.

--- src/routes/authentication/auth-routes.js
+++ src/routes/authentication/auth-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { basicLoginController } = require('../../controllers/authentication/basic-login-controller');
-const { validateLogin } = require('../../middlewares/authentication-middlewares');
+const { refreshTokenController } = require('../../controllers/authentication/refresh-token-controller');
+const { validateLogin, validateRefreshToken } = require('../../middlewares/authentication-middlewares');
 
 const router = express.Router();
 
 router.post('/basic-login', validateLogin, basicLoginController);
+router.post('/refresh-token', validateRefreshToken, refreshTokenController);
 
 module.exports = router;
