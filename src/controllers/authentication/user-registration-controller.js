2c6cebe541774fc653181ecf18509aa3727190a5
yomZsamora <samaurah@gmail.com>
2024-08-29 10:39:46 +0300

    Finalized controller for user registration.

--- src/controllers/authentication/user-registration-controller.js
+++ src/controllers/authentication/user-registration-controller.js
@@ -1,8 +1,20 @@
-
+const User = require('../../models/authentication/user');
+const { ApiResponse } = require('../../utils/responses');
 
 const userRegistrationController = async (req, res, next) => {
+    
     try {
-
+        const { email, firstName, lastName, password } = req.body;
+        const user = User.create({ email, firstName, lastName, password });
+        const apiResponse = new ApiResponse();
+        apiResponse.message = `${email} has been successfully registered.`;
+        apiResponse.data = {
+            userId: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            lastLogin: user.lastLogin,
+        }
+        res.status(apiResponse.code).json(apiResponse);
     } catch (error) {
         next(error);
     }
