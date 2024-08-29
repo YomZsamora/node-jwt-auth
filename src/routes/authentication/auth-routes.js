a353faae704ba32496fc687880ef4efd9f95d6f7
yomZsamora <samaurah@gmail.com>
2024-08-29 10:46:24 +0300

    Configured user registration route.

--- src/routes/authentication/auth-routes.js
+++ src/routes/authentication/auth-routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { basicLoginController } = require('../../controllers/authentication/basic-login-controller');
 const { refreshTokenController } = require('../../controllers/authentication/refresh-token-controller');
-const { validateLogin, validateRefreshToken } = require('../../middlewares/authentication-middlewares');
+const { userRegistrationController } = require('../../controllers/authentication/user-registration-controller');
+const { validateLogin, validateRefreshToken, validateUserRegistration } = require('../../middlewares/authentication-middlewares');
 
 const router = express.Router();
 
 router.post('/basic-login', validateLogin, basicLoginController);
 router.post('/refresh-token', validateRefreshToken, refreshTokenController);
+router.post('/user-registration', validateUserRegistration, userRegistrationController);
 
 module.exports = router;
