9f75c235ea56ecee1cd4aa0eb28ed8d518b3f060
yomZsamora <samaurah@gmail.com>
2024-08-29 09:14:53 +0300

    Removed print statements.

--- src/utils/user-helpers.js
+++ src/utils/user-helpers.js
@@ -3,14 +3,11 @@ const Permission = require('../models/authentication/permission');
 const GroupPermission = require('../models/authentication/group-permission');
 
 async function getUserGroups(userId) {
-    console.log("-------------------------CODE-CURRENT-STATUS-15---------------------------------");
     const userGroups = await UserGroup.findAll({ where: { userId } });
-    console.log("-------------------------CODE-CURRENT-STATUS-20---------------------------------");
     return userGroups.map(userGroup => userGroup.groupId);
 }
 
 async function getUserPermissions(userId) {
-    console.log("-------------------------CODE-CURRENT-STATUS-10---------------------------------");
     const groups = await getUserGroups(userId);
     const groupPermissions = await GroupPermission.findAll({ where: { groupId: groups} });
     const permissionIds = groupPermissions.map(gp => gp.permissionId);
