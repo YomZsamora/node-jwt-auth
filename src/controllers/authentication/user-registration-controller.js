b942ccdd66ac401e085f7edc7cdc0e68a57724ab
yomZsamora <samaurah@gmail.com>
2024-08-29 10:11:47 +0300

    Created controller for user registration.

---
+++ src/controllers/authentication/user-registration-controller.js
@@ -0,0 +1,11 @@
+
+
+const userRegistrationController = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = { userRegistrationController };
