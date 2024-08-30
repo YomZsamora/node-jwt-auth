39dbbefde84e5df69309e593b01d548c3587b8b1
yomZsamora <samaurah@gmail.com>
2024-08-30 15:21:19 +0300

    Updated refresh-token-controller.js

--- src/controllers/authentication/refresh-token-controller.js
+++ src/controllers/authentication/refresh-token-controller.js
@@ -13,27 +13,22 @@
 const { ApiResponse } = require('../../utils/responses');
 const { generateAccessToken, generateRefreshToken } = require('../../utils/tokens');
 
-const refreshTokenController = async (req, res, next) => {
+const refreshTokenController = async (req, res) => {
 
-    try {
-        const user = req.user;
-        const accessToken = await generateAccessToken(user);
-        const refreshToken = await generateRefreshToken(user);
-        const apiResponse = new ApiResponse();
-        apiResponse.message = "Token refreshed successfully.";
-        apiResponse.data = {
-            userId: user.id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            lastLogin: user.lastLogin,
-            accessToken,
-            refreshToken
-        };
-        return res.status(apiResponse.code).json(apiResponse);
-
-    } catch (error) {
-        next(error);
-    }
+    const user = req.user;
+    const accessToken = await generateAccessToken(user);
+    const refreshToken = await generateRefreshToken(user);
+    const apiResponse = new ApiResponse();
+    apiResponse.message = "Token refreshed successfully.";
+    apiResponse.data = {
+        userId: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        lastLogin: user.lastLogin,
+        accessToken,
+        refreshToken
+    };
+    return res.status(apiResponse.code).json(apiResponse);
 }
 
 module.exports = { refreshTokenController };
