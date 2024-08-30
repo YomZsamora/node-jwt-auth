01d9940b7d7a656566c7544e9f0afb94f35872cc
yomZsamora <samaurah@gmail.com>
2024-08-30 19:15:41 +0300

    Updated userRegistrationController.

--- src/controllers/authentication/user-registration-controller.js
+++ src/controllers/authentication/user-registration-controller.js
@@ -11,25 +11,21 @@ const { ApiResponse } = require('../../utils/responses');
 
 const userRegistrationController = async (req, res, next) => {
 
-    try {
-        const { email, firstName, lastName, password } = req.body;
-        const user = await User.create({ email, firstName, lastName, password });
-        const apiResponse = new ApiResponse();
-        apiResponse.code = 201;
-        apiResponse.message = `${email} has been successfully registered.`;
-        apiResponse.data = {
-            userId: user.id,
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            lastLogin: user.lastLogin,
-            updatedAt: user.updatedAt,
-            createdAt: user.createdAt,
-        }
-        return res.status(apiResponse.code).json(apiResponse);
-    } catch (error) {
-        next(error);
+    const { email, firstName, lastName, password } = req.body;
+    const user = await User.create({ email, firstName, lastName, password });
+    const apiResponse = new ApiResponse();
+    apiResponse.code = 201;
+    apiResponse.message = `${email} has been successfully registered.`;
+    apiResponse.data = {
+        userId: user.id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        lastLogin: user.lastLogin,
+        updatedAt: user.updatedAt,
+        createdAt: user.createdAt,
     }
+    return res.status(apiResponse.code).json(apiResponse);
 }
 
 module.exports = { userRegistrationController };
